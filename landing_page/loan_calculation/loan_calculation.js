a556f60848543778fe23f1c7a557bbe40b21cec5
MacAbrams <90845816+MacAbrams@users.noreply.github.com>
2026-02-28 11:13:53 -0500

    Update loan_calculation.js

--- landing_page/loan_calculation/loan_calculation.js
+++ landing_page/loan_calculation/loan_calculation.js
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loan calc</title>
-    <script src="./loan_calculation.js"></script>
-</head>
-<body>
-    <p>
-        testing
-    </p>
-    <div class="dropdown">
-        <button class="dropbtn">Dropdown</button>
-    </div>
-    <label for="principle"></label>
-    <input type="number" id="principle" name="principle">
-    <input type="button" id="submit" name="submit" onclick="submit()">
-</body>
-</html>
+function submit(){
+    let principle = document.getElementById("principle").value
+    console.log(principle)
+
+}
